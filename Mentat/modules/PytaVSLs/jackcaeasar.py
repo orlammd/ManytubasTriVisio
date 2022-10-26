00c1f52fac8362715d03e5b22bd1939282ae3924
Jean-Eudes <jeaneudes@ManyTubas.baronsfreaks.org>
2022-10-26 20:58:24 +0200

    Everybody lowercase!

---
+++ Mentat/modules/PytaVSLs/jackcaeasar.py
@@ -0,0 +1,29 @@
+from ..pytVSL import PytaVSL
+
+class JackCaesar(PytaVSL):
+
+    def jack_caesar_as_a_whole(self):
+        #
+        # slide_names = ['TriJC_Head', 'TriJC_Socle', 'TriJC_Tarte', 'TriJC_Souffle',
+        #         't_TriJC_Tuba', 't_TriJC_Aimant', 't_TriJC_Compas', 't_TriJC_Aspi', 't_TriJC_Taser']
+        #
+        #
+        # def closure(slide_names):
+        #
+        #     metaparam_name = "TriJC"
+        #
+        #     def getter(pos_x, pos_y, pos_z):
+        #         pos_x = self.get()
+        #
+        #     def setter(position):
+        #         for slide in slide_names: []
+        #         self.set()
+        #
+        #     self.add_meta_parameter(
+        #         metaparam_name,
+        #         ['pos_x', 'pos_y', 'pos_z', 'position'],                                                   # params
+        #         getter, setter
+        #     )
+        #
+        # closure(slide_names)
+        pass
